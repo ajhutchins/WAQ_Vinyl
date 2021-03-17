af68adea74e3e917f22fec2ddf137554607b361c
A.J. Hutchins <a.j.hutchins@Kates-MacBook-Air.local>
2021-03-17 11:38:57 -0400

    deleted the session controller

--- WAQ_Vinyl_api/server.js
+++ WAQ_Vinyl_api/server.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 // const cors = require('cors');
-const session = require('express-session');
+// const session = require('express-session');
 
 const userController = require('./controllers/users');
 const vinylController = require('./controllers/vinyl');
 const collectionController = require('./controllers/collection');
 const wishlistController = require('./controllers/wishlist');
-const sessionsController = require('./controllers/sessions_controller')
+// const sessionsController = require('./controllers/sessions_controller')
 
 const APP = express();
-const PORT = process.env.PORT || 3003;
-const mongodbURI = process.env.MONGODBURI
+const PORT = 3003;
+// const mongodbURI = process.env.MONGODBURI
 const DBNAME = 'vinyl';
 
 
@@ -42,19 +42,19 @@ APP.get('/', (req, res) => {
 
 APP.use(express.json());
 
-APP.use(
-    session({
-        secret: process.env.SECRET,
-        resave: false,
-        saveUninitialized: false
-    })
-)
+// APP.use(
+//     session({
+//         secret: process.env.SECRET,
+//         resave: false,
+//         saveUninitialized: false
+//     })
+// )
 
 APP.use('/users', userController);
 APP.use('/vinyl', vinylController);
 APP.use('/users/collection', collectionController);
 APP.use('/users/wishlist', wishlistController);
-APP.use('/sessions_controller.js', sessionsController);
+// APP.use('/sessions_controller.js', sessionsController);
 
 
 APP.listen(PORT, () => {
